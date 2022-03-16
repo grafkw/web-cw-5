503a75411b786656b3fe10acbec10c9c808ea926
grafkw <lelias2007l@gmail.com>
2022-03-16 19:13:44 +0300

    day5 cw2

--- JavaScript/assets/JavaScript.js
+++ JavaScript/assets/JavaScript.js
@@ -1,3 +1,9 @@
 alert("Elias");
 console.log("15 Years Old");
 console.log("أنا في الصف العاشر");
+
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+
+budget = 50;
+console.log(budget > 30 && budget < 80);

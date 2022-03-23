d3e2b083bfdf1ba99e4d6275383cf97eaa46d87c
grafkw <lelias2007l@gmail.com>
2022-03-23 13:15:13 +0300

    day5 cw3

--- JavaScript/assets/JavaScript.js
+++ JavaScript/assets/JavaScript.js
@@ -7,3 +7,19 @@ console.log(budget > 30 && budget < 80);
 
 budget = 50;
 console.log(budget > 30 && budget < 80);
+
+let grade = prompt("What is your grade?");
+
+if (grade >= 90) {
+  console.log("لقد حصلت على امتياز 🥳");
+} else if (grade >= 80) {
+  console.log("لقد حصلت على جيد جداً🤩");
+} else if (grade >= 70) {
+  console.log("لقد حصلت على جيد🙂");
+} else if (grade >= 60) {
+  console.log("لقد حصلت على مقبول😕");
+} else if (grade >= 50) {
+  console.log("لقد حصلت على ضعيف☹️");
+} else {
+  console.log("راسب💔");
+}
